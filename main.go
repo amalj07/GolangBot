98a0f372a83c00cb2a1821a0b366a5d52e707a25
amljs <amalavirappatt@gmail.com>
2020-11-28 22:07:41 +0530

    log errors

--- main.go
+++ main.go
@@ -14,10 +14,17 @@ var (
 	accessTokenSecret = os.Getenv("ACCESS_TOKEN_SECRET")
 )
 
+type logger struct {
+	*logrus.Logger
+}
+
 func main() {
 	anaconda.SetConsumerKey(consumerKey)
 	anaconda.SetConsumerSecret(consumerSecret)
 	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+	log := &logger{logrus.New()}
+	api.SetLogger(log)
 
 	stream := api.PublicStreamFilter(url.Values{
 		"track": []string{"#testgolangbotproject"},
@@ -45,3 +52,8 @@ func main() {
 		logrus.Infof("Retweeted: %d", t.Id)
 	}
 }
+
+func (log *logger) Critical(args ...interface{}) { log.Error(args...) }
+func (log *logger) Criticalf(format string, args ...interface{}) { log.Errorf(format, args...) }
+func (log *logger) Notice(args ...interface{}) { log.Error(args...) }
+func (log *logger) Noticef(format string, args ...interface{}) { log.Errorf(format, args...) }
